434c64e83f5b106d520e28cc3e1148ae173afd82
lysanderg13 <lysanderg13@gmail.com>
2023-10-10 20:41:45 -0500

    Adding Validations to Models

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,5 +2,7 @@ class Character < ApplicationRecord
   belongs_to :crew
   belongs_to :fruit
 
-  validates :name
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :bounty, numericality: { only_integers: true }
 end
