94b0432d9d28c321fc72087bd9a9ea9a4b09c4aa
Josiah Lopez <josiah.lopez45@yahoo.ca>
2023-10-13 22:14:57 -0500

    Fixed Search Results w/ Bootstrap

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def search
-    @query = params[:query]
+    @query = params[:search]
     @characters = Character.where('name LIKE ?', "%#{@query}%")
     @crews = Crew.where('name LIKE ?', "%#{@query}%")
     @fruits = Fruit.where('name LIKE ?', "%#{@query}%")
